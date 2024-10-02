81177de1f53ce3f899a185a1c6f13bc5e3f9352b
tijn-avontuur <bananenvla1234@gmail.com>
2024-10-02 10:32:15 +0200

    Ticket 2

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -12,7 +12,6 @@
         </x-slot:title>
     </h1>
 
-
     <?php
     $size = count($brands);
     $columns = 3;
@@ -22,37 +21,27 @@
     <div class="container">
         <!-- Example row of columns -->
         <div class="row">
-
             @foreach($brands->chunk($chunk_size) as $chunk)
                 <div class="col-md-4">
-
                     <ul>
                         @foreach($chunk as $brand)
-
                             <?php
                             $current_first_letter = strtoupper(substr($brand->name, 0, 1));
-
                             if (!isset($header_first_letter) || (isset($header_first_letter) && $current_first_letter != $header_first_letter)) {
                                 echo '</ul>
-						<h2>' . $current_first_letter . '</h2>
-						<ul>';
+                        <h2>' . $current_first_letter . '</h2>
+                        <ul>';
                             }
-                            $header_first_letter = $current_first_letter
+                            $header_first_letter = $current_first_letter;
                             ?>
-
                             <li>
                                 <a href="/{{ $brand->id }}/{{ $brand->getNameUrlEncodedAttribute() }}/">{{ $brand->name }}</a>
                             </li>
                         @endforeach
                     </ul>
-
                 </div>
-                <?php
-                unset($header_first_letter);
-                ?>
+                <?php unset($header_first_letter); ?>
             @endforeach
-
         </div>
-
     </div>
 </x-layouts.app>
